584a667882cd2f880204eb0178b411ce7f096f3b
emzad <66334172+emzad@users.noreply.github.com>
2021-04-09 13:48:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-
 window.addEventListener("scroll", function(event) {
   
     var top = this.scrollY;
     
-    if((100-(top/1.25)) > 5 )
+    if(top > 100) {
+        $("div").filter(".salutation").css("position", "absolute").css("margin-left", "5vw");
+        $("div").filter(".welcome-body").css("position", "absolute").css("margin-left", "10vw");
+    }
+    else if(top <100 )
     {
-        $("div").filter(".salutation").css("position", "absolute").css("margin-left", (105-(top/1.25)) + "vw");
-        $("div").filter(".welcome-body").css("position", "absolute").css("margin-left", (110-(top/1.25)) + "vw");
+        $("div").filter(".salutation").css("position", "absolute").css("margin-left", (100-(top/1.25)) + "vw");
+        $("div").filter(".welcome-body").css("position", "absolute").css("margin-left", (105-(top/1.25)) + "vw");
     }
 
 
