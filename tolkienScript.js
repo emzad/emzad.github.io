0930373a0de0a70863fb3b078fa268ac21fa2e92
emzad <66334172+emzad@users.noreply.github.com>
2021-05-13 00:03:04 -0400

    Add files via upload

---
+++ tolkienScript.js
@@ -0,0 +1,11 @@
+//document.getElementById("youngTolkien").addEventListener("mouseenter", youngDate);
+
+function youngDate() {
+    document.getElementById("youngTolkien").setAttribute('src','https://cdn.britannica.com/65/66765-050-63A945A7/JRR-Tolkien.jpg');
+}
+//
+//document.getElementById("youngTolkien").hover(function() {
+//    this.attr('src', 'https://cdn.britannica.com/65/66765-050-63A945A7/JRR-Tolkien.jpg');
+//  }, function() {
+//    $(this).attr('src', '/folder/image1.jpg');
+//  });
