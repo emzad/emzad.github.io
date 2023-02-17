6994729bf91d5d79242301a4ee5cad8b99c85b7e
emzad <66334172+emzad@users.noreply.github.com>
2023-02-17 01:29:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+document.querySelector('.banner button').addEventListener('click', () => {
+    document.querySelector('.banner').style.display = 'none';
+    document.querySelector('.slideshow-wrapper').style.cssText = 'opacity: 1; visibility: visible';
+    document.querySelector('.slideshow').style.animation = 'slideshow 30s infinite';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
